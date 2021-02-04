7ad3682e506fb47a50af0bf8cd6c4c5b535d1618
Daniel Costa <daniel.costa@sayduck.com>
2021-02-04 15:20:23 +0200

    bugfix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::API
   def _render
     response.set_header('x-ratelimit-limit', @service[:limit])
     response.set_header('x-ratelimit-remaining', @service[:remaining])
-    response.set_header('x-cached', @service[:cached])
     render json: @service[:data], status: :ok
   end
 end
